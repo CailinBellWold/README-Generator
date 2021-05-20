3cbe3002156125f4863bf3bf70d0531fe7428da3
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-05-19 22:09:44 -0700

    Input Questions, Initial Functions.

--- index.js
+++ index.js
@@ -1,13 +1,112 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require('util');
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Input your project description.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Input your project installation instructions.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Input your project usage information.',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Input your project contribution guidelines.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Input your project test instructions.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license applies to your project?',
+            choices: ['GPL', 'MIT', 'Apache License 2.0', 'BSD'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+const generateMD = (answers) =>
+# Title 
+${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [License](#License)
+- [Questions](#Questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+${answers.license}
+
+## Questions
+${answers.github}
+${answers.email}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+        .then((answers) => writeFileAsync('README.md', generateMD(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
